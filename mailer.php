6350e3388e0950d1fafbf242e8e30e3ab7f263fb
diasdev02 <diasdev@icloud.com>
2024-11-07 04:36:26 +0500

    change mail

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "csealtaf@gmail.com";
+        $recipient = "diasdev@icloud.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
